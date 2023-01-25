b911c238226b41ea09592b43c5f755926c0e8aa0
Murdo Meiring <murdo.meiring@wearetipjar.com>
2023-01-25 16:40:04 +0200

    I think I added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "xxxxxxxx-xxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxx",
-    storageBucket: "xxxxxxxx",
-    messagingSenderId: "xxxxxx",
-    appId: "xxxxx",
-    measurementId: "xxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyA2tNy1xoqddr9RMDX03o7hqLW3Ky6MKOU",
+    authDomain: "taskdescriptorpersonal.firebaseapp.com",
+    projectId: "taskdescriptorpersonal",
+    storageBucket: "taskdescriptorpersonal.appspot.com",
+    messagingSenderId: "684375885444",
+    appId: "1:684375885444:web:a97ee98cf37a62bb13e4d1",
+    measurementId: "G-09M03FY2GS"
   }
 };
